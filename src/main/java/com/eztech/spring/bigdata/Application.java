f47df8bd7e881a62e043e467e908d334f13f0baa
Jia ZHOU <emeerzhou@gmail.com>
2016-01-17 16:25:23 +0100

    Integration of spring boot

---
+++ src/main/java/com/eztech/spring/bigdata/Application.java
@@ -0,0 +1,21 @@
+package com.eztech.spring.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Big data Application
+ *
+ * @author Jia ZHOU
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
