d48899c51e82b8cc6f7c0d206b947d685f3ba0ab
Jia ZHOU <jia.zhou@orchestra.eu>
2017-05-11 14:17:24 +0200

    Update cassandra unit test

--- src/test/java/com/eztech/spring/bigdata/controller/HomeControllerTest.java
+++ src/test/java/com/eztech/spring/bigdata/controller/HomeControllerTest.java
@@ -1,6 +1,9 @@
 package com.eztech.spring.bigdata.controller;
 
-import com.eztech.spring.bigdata.controller.HomeController;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,35 +14,33 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 /**
- * 
+ *
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HomeControllerTest {
 
-	private MockMvc mvc;
+    private MockMvc mvc;
 
- /**
-  * 
-  */
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
-	}
 
- /**
-  * 
-  */
-	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello, spring big data!")));
-	}
+    /**
+     *
+     */
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    public void findCustomer() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello, spring big data!")));
+    }
 }
 
--- src/test/java/com/eztech/spring/bigdata/controller/cassandra/CassandraControllerTest.java
+++ src/test/java/com/eztech/spring/bigdata/controller/cassandra/CassandraControllerTest.java
@@ -1,28 +1,41 @@
 package com.eztech.spring.bigdata.controller.cassandra;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import com.eztech.spring.bigdata.controller.HomeController;
 import org.cassandraunit.spring.CassandraDataSet;
 import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
 import org.cassandraunit.spring.EmbeddedCassandra;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({CassandraUnitTestExecutionListener.class})
 @CassandraDataSet(value = {"simple.cql"})
 @EmbeddedCassandra
+@SpringBootTest
 public class CassandraControllerTest {
 
     private Session session;
+
+    private MockMvc mvc;
+
 
     @Before
     public void setUp() {
@@ -33,11 +46,21 @@ public class CassandraControllerTest {
 
         session = cluster.connect("cassandra_unit_keyspace");
 
+        mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
     }
+
 
     @Test
     public void should_have_started_and_execute_cql_script() throws Exception {
-        ResultSet result = session.execute("select * from customer WHERE first_name = 'eric' ALLOW FILTERING");
+        ResultSet result = session.execute("select * from customer WHERE emails contains 'test@hotmail.com' ALLOW FILTERING");
         assertThat(result.iterator().next().getString("last_name"), is("zhou"));
+    }
+
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/cassandra/customer/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello, spring big data!")));
     }
 }
