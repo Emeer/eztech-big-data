f8ff4cc7e1425f96622a835c42eb024b3436a5b2
Jia ZHOU <emeerzhou@gmail.com>
2017-05-13 10:50:13 +0200

    Update cassnadra repo test

--- src/test/java/com/eztech/spring/bigdata/controller/cassandra/CassandraRepoTest.java
+++ src/test/java/com/eztech/spring/bigdata/controller/cassandra/CassandraRepoTest.java
@@ -1,38 +1,45 @@
 package com.eztech.spring.bigdata.controller.cassandra;
 
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import com.eztech.spring.bigdata.persistence.domain.cassandra.Customer;
 import com.eztech.spring.bigdata.persistence.repository.cassandra.CustomerCassandraRepository;
+import com.google.common.collect.Sets;
+import org.assertj.core.util.Lists;
+import org.assertj.core.util.Maps;
 import org.cassandraunit.spring.CassandraDataSet;
-import org.cassandraunit.spring.CassandraUnit;
-import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener;
+import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
+import org.cassandraunit.spring.EmbeddedCassandra;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.repository.support.CassandraRepositoryFactory;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  *
  */
-@TestExecutionListeners(listeners = {CassandraUnitDependencyInjectionTestExecutionListener.class})
+@TestExecutionListeners({CassandraUnitTestExecutionListener.class, DependencyInjectionTestExecutionListener.class})
 @CassandraDataSet(value = {"simple.cql"})
-@CassandraUnit
+@EmbeddedCassandra
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
 public class CassandraRepoTest {
 
     private Session session;
 
-    @Autowired
     private CustomerCassandraRepository customerCassandraRepository;
 
 
@@ -44,7 +51,10 @@ public class CassandraRepoTest {
                 .build();
 
         session = cluster.connect("cassandra_unit_keyspace");
-        MockitoAnnotations.initMocks(this);
+
+        CassandraOperations cassandraOps = new CassandraTemplate(session);
+        customerCassandraRepository = new CassandraRepositoryFactory(cassandraOps).getRepository(CustomerCassandraRepository.class);
+
     }
 
 
@@ -53,4 +63,27 @@ public class CassandraRepoTest {
         ResultSet result = session.execute("select * from customer WHERE emails contains 'test@hotmail.com' ALLOW FILTERING");
         assertThat(result.iterator().next().getString("last_name"), is("zhou"));
     }
+
+
+    @Test
+    public void testSave() {
+        Customer customer = new Customer();
+        customer.setId(UUID.randomUUID());
+        customer.setFirstName("jia");
+        customer.setLastName("zhou");
+        customer.setEmails(Sets.newHashSet("test1@gmail.com", "test2@hotmail.com"));
+        customer.setTopScores(Lists.newArrayList(1, 2, 3));
+        customer.setTodo(Maps.newHashMap(new Date(), "today"));
+
+        Customer cratedCustomer = customerCassandraRepository.save(customer);
+        assertThat(cratedCustomer.getId(), is(customer.getId()));
+    }
+
+    @Test
+    public void testFindByFirstName() {
+        List<Customer> customers = customerCassandraRepository.findByFirstName("eric");
+        assertThat(customers.isEmpty(), is(false));
+        assertThat(customers.get(0).getLastName(), is("zhou"));
+    }
+
 }
