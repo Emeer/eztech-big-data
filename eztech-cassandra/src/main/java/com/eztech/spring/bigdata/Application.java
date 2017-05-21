fb36b3fab0e4281cef098a19dc301b1869581575
Jia ZHOU <emeerzhou@gmail.com>
2017-05-21 19:58:00 +0200

    Update config

--- eztech-cassandra/src/main/java/com/eztech/spring/bigdata/Application.java
+++ eztech-cassandra/src/main/java/com/eztech/spring/bigdata/Application.java
@@ -4,7 +4,6 @@ import com.eztech.spring.bigdata.config.RedisCacheConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
