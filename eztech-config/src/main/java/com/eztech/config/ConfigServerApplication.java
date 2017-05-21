ce72e90831351a11c51cfb33b5ab598ad56a08ec
Jia ZHOU <emeerzhou@gmail.com>
2017-05-21 18:23:57 +0200

    Update to cloud

---
+++ eztech-config/src/main/java/com/eztech/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.eztech.net.cokey.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Start cokey config server.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
